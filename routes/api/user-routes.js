0bcff33a7e1c8b5edc74e1cca97ff0db0ec72153
JustinSnyder611 <JustinSnyder611@gmail.com>
2022-12-08 14:50:00 -0600

    added routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,4 +1,22 @@
 const router = require('express').Router();
 
+const {
+    getUsers,
+    createUser,
+    getSingleUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+} = require('.../controllers/userController');
+
 // api/users
 router.route('/users').get(getUsers).post(createUser);
+
+// api/users/:userId
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
+// api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+
+module.exports = router;
